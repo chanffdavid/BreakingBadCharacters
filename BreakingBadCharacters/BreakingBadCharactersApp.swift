aadd72811f8df85e0c972990a212c9b98308e7bb
iovoi-dev <82698882+iovoi-dev@users.noreply.github.com>
2021-09-30 09:15:59 +0100

    Initial Commit

---
+++ BreakingBadCharacters/BreakingBadCharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakingBadCharactersApp.swift
+//  BreakingBadCharacters
+//
+//  Created by iovoi on 30/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct BreakingBadCharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
